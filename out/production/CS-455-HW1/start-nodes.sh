653b47d4b8dd7599f3e1170101507a2ed5148b31
Joshua Burris <joshtb@rams.colostate.edu>
2020-02-07 14:42:47 -0700

    setup-overlay now computes the correct distance nodes

--- out/production/CS-455-HW1/start-nodes.sh
+++ out/production/CS-455-HW1/start-nodes.sh
@@ -2,7 +2,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 JAR_PATH="$DIR/conf/:$DIR/out/artifacts/CS_455_HW1_jar/CS-455-HW1.jar"
 MACHINE_LIST="$DIR/conf/machine_list"
 
-SCRIPT="java -cp $JAR_PATH cs455.overlay.node.MessagingNode hong-kong 1024"
+SCRIPT="java -cp $JAR_PATH cs455.overlay.node.MessagingNode dover 1024"
 COMMAND='gnome-terminal --geometry=200x40'
 
 for machine in `cat $MACHINE_LIST`
